115edb19a6ed738b3595d4162bb1847445a0ca5e
khjf149_azu <khjf149@astrazeneca.net>
2024-11-30 20:48:29 +0100

    added sns sqs

--- infrastructure/bin/cloudx-aws.ts
+++ infrastructure/bin/cloudx-aws.ts
@@ -3,7 +3,11 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { DeployWebAppStack } from "../lib/deploy-web-app-stack";
 import { ProductStack } from "../lib/lambda/product-stack";
+import { ProductSqsStack } from "../lib/product-sqs/product-sqs-stack";
+import { ProductSnsStack } from "../lib/product-sns/product-sns-stack";
 
 const app = new cdk.App();
 new DeployWebAppStack(app, "DeployWebAppStack", {});
 new ProductStack(app, "ProductStack", {});
+new ProductSqsStack(app, "ProductSqsStack", {});
+new ProductSnsStack(app, "ProductSnsStack", {});
