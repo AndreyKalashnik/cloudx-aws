c3ccc1d7d2b67ff589c6a7cf9b913d9caf9cf84b
khjf149_azu <khjf149@astrazeneca.net>
2024-12-01 21:10:21 +0100

    added Auth through lambda

--- infrastructure/bin/cloudx-aws.ts
+++ infrastructure/bin/cloudx-aws.ts
@@ -2,10 +2,11 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { DeployWebAppStack } from "../lib/deploy-web-app-stack";
-import { ProductStack } from "../lib/lambda/product-stack";
+import { ProductStack } from "../lib/product-stack";
 import { ImportServiceStack } from "../lib/import-service-stack";
 import { ProductSqsStack } from "../lib/product-sqs/product-sqs-stack";
 import { ProductSnsStack } from "../lib/product-sns/product-sns-stack";
+import { AuthorizerStack } from "../lib/authorizer-stack";
 
 const app = new cdk.App();
 new DeployWebAppStack(app, "DeployWebAppStack", {});
@@ -13,3 +14,4 @@ new ProductStack(app, "ProductStack", {});
 new ImportServiceStack(app, "ImportServiceStack", {});
 new ProductSqsStack(app, "ProductSqsStack", {});
 new ProductSnsStack(app, "ProductSnsStack", {});
+new AuthorizerStack(app, "AuthorizerStack");
